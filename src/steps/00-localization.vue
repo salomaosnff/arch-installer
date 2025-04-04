9b58ec3c75d4f0be9de5a4077f995ba63974e068
Isaac Henrique <isaac@isaacsolutions.tech>
2025-04-04 19:19:45 -0300

    feat: keyboard layout

--- src/steps/00-localization.vue
+++ src/steps/00-localization.vue
@@ -1,16 +1,28 @@
 <script setup lang="ts">
 import { useInstallerStepper } from "../composables/useStepper";
-import { readTextFileLines } from "@tauri-apps/plugin-fs";
+import { readTextFile, readTextFileLines } from "@tauri-apps/plugin-fs";
 
 useWindowTitle({
   title: "Bem-vindo ao Arch Linux",
   titleTemplate: (title) => title,
 });
 
-const language = ref("pt_BR");
-const keymap = ref("br-abnt2");
-const useSameKeymap = ref(true);
+interface KeyboardVariant {
+  code: string;
+  title: string;
+}
 
+interface KeyboardLayout {
+  code: string;
+  title: string;
+  variants: Array<KeyboardVariant>
+}
+
+const language = ref("pt_BR");
+const useSameKeymap = ref(true);
+const keymap = ref<{ layout: KeyboardLayout | null, variant?: string; }>({ layout: null, variant: "" });
+
+const keymaps = ref<Array<KeyboardLayout>>([]);
 const languages = ref<Array<{ code: string; title: string }>>([]);
 
 const { nextLink } = useInstallerStepper();
@@ -39,254 +51,35 @@ readTextFileLines("/etc/locale.gen").then(async (rows) => {
 });
 
 async function readKeymaps() {
-  return `3l
-ANSI-dvorak
-adnw
-amiga-de
-amiga-us
-apple-a1048-sv
-apple-a1243-sv
-apple-a1243-sv-fn-reverse
-apple-internal-0x0253-sv
-apple-internal-0x0253-sv-fn-reverse
-applkey
-atari-de
-atari-se
-atari-uk-falcon
-atari-us
-azerty
-backspace
-bashkir
-be-latin1
-bg-cp1251
-bg-cp855
-bg_bds-cp1251
-bg_bds-utf8
-bg_pho-cp1251
-bg_pho-utf8
-bone
-br-abnt
-br-abnt2
-br-latin1-abnt2
-br-latin1-us
-by
-by-cp1251
-bywin-cp1251
-ca
-carpalx
-carpalx-full
-cf
-colemak
-croat
-ctrl
-cz
-cz-cp1250
-cz-lat2
-cz-lat2-prog
-cz-qwertz
-cz-us-qwertz
-de
-de-latin1
-de-latin1-nodeadkeys
-de-mobii
-de_CH-latin1
-de_alt_UTF-8
-defkeymap
-defkeymap_V1.0
-dk
-dk-latin1
-dvorak
-dvorak-ca-fr
-dvorak-de
-dvorak-es
-dvorak-fr
-dvorak-l
-dvorak-la
-dvorak-no
-dvorak-programmer
-dvorak-r
-dvorak-ru
-dvorak-sv-a1
-dvorak-sv-a5
-dvorak-uk
-dvorak-ukp
-emacs
-emacs2
-en
-es
-es-cp850
-es-olpc
-et
-et-nodeadkeys
-euro
-euro1
-euro2
-fa
-fi
-fr
-fr-bepo
-fr-bepo-latin9
-fr-latin1
-fr-latin9
-fr-pc
-fr_CH
-fr_CH-latin1
-gr
-gr-pc
-hu
-hu101
-ie
-il
-il-heb
-il-phonetic
-is-latin1
-is-latin1-us
-it
-it-ibm
-it2
-jp106
-kazakh
-keypad
-koy
-ky_alt_sh-UTF-8
-kyrgyz
-la-latin1
-lt
-lt.baltic
-lt.l4
-lv
-lv-tilde
-mac-be
-mac-de-latin1
-mac-de-latin1-nodeadkeys
-mac-de_CH
-mac-dk-latin1
-mac-dvorak
-mac-es
-mac-euro
-mac-euro2
-mac-fi-latin1
-mac-fr
-mac-fr-legacy
-mac-fr_CH-latin1
-mac-it
-mac-no-latin1
-mac-pl
-mac-pt-latin1
-mac-se
-mac-template
-mac-uk
-mac-us
-mk
-mk-cp1251
-mk-utf
-mk0
-mod-dh-ansi-us
-mod-dh-ansi-us-awing
-mod-dh-ansi-us-fatz
-mod-dh-ansi-us-fatz-wide
-mod-dh-ansi-us-wide
-mod-dh-iso-uk
-mod-dh-iso-uk-wide
-mod-dh-iso-us
-mod-dh-iso-us-wide
-mod-dh-matrix-us
-neo
-neoqwertz
-nl
-nl2
-no
-no-latin1
-pc110
-pl
-pl1
-pl2
-pl3
-pl4
-pt-latin1
-pt-latin9
-pt-olpc
-ro
-ro_std
-ro_win
-ru
-ru-cp1251
-ru-ms
-ru-yawerty
-ru1
-ru2
-ru3
-ru4
-ru_win
-ruwin_alt-CP1251
-ruwin_alt-KOI8-R
-ruwin_alt-UTF-8
-ruwin_alt_sh-UTF-8
-ruwin_cplk-CP1251
-ruwin_cplk-KOI8-R
-ruwin_cplk-UTF-8
-ruwin_ct_sh-CP1251
-ruwin_ct_sh-KOI8-R
-ruwin_ct_sh-UTF-8
-ruwin_ctrl-CP1251
-ruwin_ctrl-KOI8-R
-ruwin_ctrl-UTF-8
-se-fi-ir209
-se-fi-lat6
-se-ir209
-se-lat6
-sg
-sg-latin1
-sg-latin1-lk450
-sk-prog-qwerty
-sk-prog-qwertz
-sk-qwerty
-sk-qwertz
-slovene
-sr-cy
-sr-latin
-sun-pl
-sun-pl-altgraph
-sundvorak
-sunkeymap
-sunt4-es
-sunt4-fi-latin1
-sunt4-no-latin1
-sunt5-cz-us
-sunt5-de-latin1
-sunt5-es
-sunt5-fi-latin1
-sunt5-fr-latin1
-sunt5-ru
-sunt5-uk
-sunt5-us-cz
-sunt6-uk
-sv-latin1
-tj_alt-UTF8
-tr_f-latin5
-tr_q-latin5
-tralt
-trf
-trf-fgGIod
-trq
-ttwin_alt-UTF-8
-ttwin_cplk-UTF-8
-ttwin_ct_sh-UTF-8
-ttwin_ctrl-UTF-8
-ua
-ua-cp1251
-ua-utf
-ua-utf-ws
-ua-ws
-uk
-unicode
-us
-us-acentos
-us1
-wangbe
-wangbe2
-windowkeys`;
+  const content = await readTextFile("/usr/share/X11/xkb/rules/base.xml");
+  const doc = new DOMParser().parseFromString(content, "application/xml");
+
+  const layoutsTags = doc.querySelectorAll("layoutList > layout");
+
+  for (const layout of layoutsTags) {
+    const layoutName = layout.querySelector("configItem > name")?.textContent;
+    const layoutDescription = layout.querySelector("configItem > description")?.textContent;
+
+    if (!layoutName || !layoutDescription) {
+      continue;
+    }
+
+    const layoutVariants = Array.from(layout.querySelectorAll("variantList > variant"), (item) => ({
+      code: item.querySelector("configItem > name")?.textContent!,
+      title: item.querySelector("configItem > description")?.textContent!,
+    }));
+
+    keymaps.value.push({
+      code: layoutName,
+      title: layoutDescription,
+      variants: layoutVariants,
+    });
+  }
+
+  keymaps.value.sort((a, b) => a.title.localeCompare(b.title));
 }
+
+readKeymaps();
 </script>
 
 <template>
@@ -301,7 +94,7 @@ windowkeys`;
       <label>
         <p>Idioma</p>
         <select v-model="language" class="w-full">
-          <option v-for="lang in languages" :value="lang.code">
+          <option v-for="lang in languages" :key="lang.code" :value="lang.code">
             {{ lang.title }}
           </option>
         </select>
@@ -309,11 +102,22 @@ windowkeys`;
 
       <label>
         <p>Layout do Teclado</p>
-        <select v-model="keymap" class="w-full">
-          <option value="br-abnt2">Português (Brasil - ABNT2)</option>
-          <option value="us">English (US)</option>
-          <option value="es">Español (España)</option>
-          <option value="fr">Français (France)</option>
+        <select v-model="keymap.layout" class="w-full">
+          <option v-for="item in keymaps" :key="item.code" :value="item">{{ item.title }}</option>
+        </select>
+      </label>
+
+      <pre>
+        {{ {
+          layout: keymap.layout?.code,
+          variant: keymap.variant,
+        } }}
+      </pre>
+
+      <label v-if="keymap.layout?.variants.length">
+        <p>Variante do Teclado</p>
+        <select v-model="keymap.variant" class="w-full">
+          <option v-for="item in keymap.layout.variants" :key="item.code" :value="item.code">{{ item.title }}</option>
         </select>
       </label>
 
