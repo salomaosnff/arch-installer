07746f81ad69983f049b14f1d4335cf6095fa3ad
Erisnilton Freitas <erisniltongf@gmail.com>
2025-07-09 02:17:58 -0300

    feat: add xterm.js terminal component and enhance installer service tasks

--- src/steps/08-finish.vue
+++ src/steps/08-finish.vue
@@ -1,11 +1,30 @@
 <script setup lang="ts">
 import { exit } from "@tauri-apps/plugin-process";
 import Button from "primevue/button";
-import StepImage from "../svg/completing.vue";
+import { useTemplateRef, watch } from "vue";
+import { InstallerService } from "../services/installer-service";
 import { isInstalling } from "../stores/installer";
+import StepImage from "../svg/completing.vue";
 import { execCommand } from "../utils/sh";
 
 isInstalling.value = false;
+const { logs } = InstallerService;
+const log = useTemplateRef("log");
+
+watch(
+  logs,
+  () => {
+    if (log.value) {
+      log.value.scroll({
+        top: log.value.scrollHeight,
+        behavior: "instant",
+      });
+    }
+  },
+  {
+    flush: "post",
+  }
+);
 
 function reboot() {
   return execCommand("reboot");
@@ -13,21 +32,25 @@ function reboot() {
 </script>
 
 <template>
-  <div
-    class="prose w-full max-w-1024px mx-auto text-center pa-8 h-screen overflow-hidden"
-  >
-    <StepImage class="h-380px mx-auto mb-8" />
-    <h4 class="mb-2">Instalação Concluída</h4>
-    <p class="mb-8">
-      Você pode continuar experimentando o live CD ou reiniciar seu novo
-      sistema.
-    </p>
-    <Button class="block mb-4" label="Reiniciar" @click="reboot" /><br />
-    <Button
-      class="block"
-      label="Continuar experimentado"
-      severity="secondary"
-      @click="exit(0)"
-    />
+  <div class="flex flex-col w-full max-w-1024px mx-auto pa-8 h-screen overflow-hidden">
+    <div
+      class="prose flex-1 w-full max-w-1024px mx-auto text-center pa-8 overflow-hidden"
+    >
+      <StepImage class="h-240px mx-auto mb-8" />
+      <h4 class="mb-2">Instalação Concluída</h4>
+      <p class="mb-8">
+        Você pode continuar experimentando o live CD ou reiniciar seu novo
+        sistema.
+      </p>
+      <Button class="block mb-4" label="Reiniciar" @click="reboot" /><br />
+      <Button
+        class="block"
+        label="Continuar experimentado"
+        severity="secondary"
+        @click="exit(0)"
+      />
+    </div>
+    <pre ref="log" class="flex-1 min-h-80px overflow-auto bg-black color-white pa-4 text-3 rounded-md">{{ logs }}</pre>
+
   </div>
 </template>
